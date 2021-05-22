8e26992d85671fecb1651f34c4a1f1872294f733
Sidharth <sidharth@ndimensionz.com>
2021-05-22 11:03:34 +0530

    Authentication services added

--- invoice_link_generator/invoice_link/admin.py
+++ invoice_link_generator/invoice_link/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import ClientInvoice, InvoiceLink
 # Register your models here.
+
+admin.site.site_header = "Client Invoice Generator Admin Portal"
+admin.site.site_title = "CIG Portal"
+admin.site.index_title = "Welcome to Client Invoice Generator Admin Portal"
+
+
+admin.site.register(ClientInvoice)
+admin.site.register(InvoiceLink)
